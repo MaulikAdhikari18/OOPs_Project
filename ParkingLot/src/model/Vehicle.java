c82392451a225492260f0028a192b16288cfe673
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-01-26 20:06:59 +0530

    Added Vehicle abstraction and VehicleType enum

---
+++ ParkingLot/src/model/Vehicle.java
@@ -0,0 +1,21 @@
+package model;
+
+import enums.VehicleType;
+
+public abstract class Vehicle {
+    protected String licensePlate;
+    protected VehicleType type;
+
+    public Vehicle(String licensePlate, VehicleType type) {
+        this.licensePlate = licensePlate;
+        this.type = type;
+    }
+
+    public VehicleType getType() {
+        return type;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+}
