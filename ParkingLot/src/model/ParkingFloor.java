2047445fe35f7865381c68a2792d860d7af2b873
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-02-27 22:45:36 +0530

    ParkingFloor

---
+++ ParkingLot/src/model/ParkingFloor.java
@@ -0,0 +1,33 @@
+package model;
+
+import enums.VehicleType;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParkingFloor {
+
+    private final int floorNumber;
+    private final List<ParkingSpot> spots;
+
+    public ParkingFloor(int floorNumber) {
+        this.floorNumber = floorNumber;
+        this.spots = new ArrayList<>();
+    }
+
+    public void addSpot(ParkingSpot spot) {
+        spots.add(spot);
+    }
+
+    public ParkingSpot findAvailableSpot(Vehicle vehicle) {
+        for (ParkingSpot spot : spots) {
+            if (spot.canPark(vehicle)) {
+                return spot;
+            }
+        }
+        return null; // No available spot
+    }
+
+    public int getFloorNumber() {
+        return floorNumber;
+    }
+}
