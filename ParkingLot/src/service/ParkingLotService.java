cf4360cc05a07932b9917467079661ebc94c990f
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-03-05 13:58:21 +0530

    Hourly fees

--- ParkingLot/src/service/ParkingLotService.java
+++ ParkingLot/src/service/ParkingLotService.java
@@ -7,6 +7,9 @@ import model.Vehicle;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class ParkingLotService {
 
     private final List<ParkingFloor> floors;
@@ -45,22 +48,32 @@ public class ParkingLotService {
         for (ParkingFloor floor : floors) {
             for (ParkingSpot spot : floor.getSpots()) {
 
-                if (spot.isOccupied() &&
-                        spot.getParkedVehicle().getLicensePlate()
-                                .equals(vehicle.getLicensePlate())) {
+                if (spot.isOccupied() &&spot.getParkedVehicle().getLicensePlate().equals(vehicle.getLicensePlate())) {
+                    LocalDateTime entryTime = spot.getEntryTime();
+                    LocalDateTime exitTime = LocalDateTime.now();
+
+                    long hours = Duration.between(entryTime, exitTime).toHours();
+                    if (hours == 0) {
+                        hours = 1; // minimum charge
+                    }
+
+                    long fee = hours * 50;
+
+                    System.out.println("\n===== PARKING BILL =====");
+                    System.out.println("Vehicle: " + vehicle.getLicensePlate());
+                    System.out.println("Entry Time: " + entryTime);
+                    System.out.println("Exit Time: " + exitTime);
+                    System.out.println("Total Hours: " + hours);
+                    System.out.println("Total Fee: ₹" + fee);
 
                     spot.removeVehicle();
-                    System.out.println("Vehicle removed from Floor "
-                            + floor.getFloorNumber()
-                            + ", Spot "
-                            + spot.getSpotId());
+
                     return true;
                 }
             }
         }
 
-        System.out.println("Vehicle not found: "
-                + vehicle.getLicensePlate());
+        System.out.println("Vehicle not found.");
         return false;
     }
 }
